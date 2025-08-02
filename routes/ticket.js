d196f789a99d9d50e6054baa171a9f7bd0afb3bf
Ritik rikhi <rikhiritik8@gmail.com>
2025-08-02 14:23:59 +0530

    Delete routes directory

--- routes/ticket.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ticketController = require('../controllers/ticketController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/create', protect, ticketController.createTicket);
-
-module.exports = router;
